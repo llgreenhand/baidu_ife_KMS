8a423ad14b20eb7f4b3955ce148bb97ef32c285a
Mrsususu <lisusie@126.com>
2015-12-10 21:49:37 +0800

    a little js part

--- lsnote/js/main.js
+++ lsnote/js/main.js
@@ -3,3 +3,8 @@ var windowHeight = $(window).height();
 
 $("#left").height(windowHeight + "px");
 $("#center").height(windowHeight + "px");
+
+$("#left .add").on("click",function(){
+	$("#left li:last").after("<li><input type=\"text\" placeholder=\"笔记本名称\"/></li>");
+	$("#left li:last").attr("id","newwrite");
+});
